baf3dec4b1330df6f69f6a29c6bfb747c9ddf9a9
Ben <bennstratton7@gmail.com>
2023-12-12 16:20:46 -0500

    bug fixes in processing and begun NN

--- main.py
+++ main.py
@@ -1,11 +1,28 @@
 import numpy as np
 import matplotlib.pyplot as plt
+import process_data
+# from sys import maxsize
+# np.set_printoptions(threshold=maxsize)
 
+image_channels = 3
+input_size = process_data.size_of_processed_image * process_data.size_of_processed_image * image_channels
+hidden_size = 224
+output_size = 2
+
+weights_hidden = np.random.randn(input_size, hidden_size)
+
+print(weights_hidden)
+
+#activation functions
 def sigmoid(x):
     return 1/(1+np.exp(-x))
-
 def relu(x):
     return max(0, x)
-
 def myactivation(x):
     return pow(x, 2)/2
+
+#loss functions
+def cross_entropy(Y, P):
+    epsilon = 1e-10
+    loss = -np.mean(np.sum(Y*np.log(P + epsilon), axis=1))
+    return loss
--- process_data.py
+++ process_data.py
@@ -9,22 +9,29 @@ annotations_dir_name = 'test_annotations'
 images_dir_name = 'test_images'
 annotations_dir = os.path.join(data_location, annotations_dir_name)
 images_dir = os.path.join(data_location, images_dir_name)
-size_of_processed_image = 24
+size_of_processed_image = 224
 normalized_image_width, normalized_image_height = (size_of_processed_image, size_of_processed_image)
+images_data = {}
 
-def preprocess_image(im_path, b_box):
-    im = Image.open(im_path)
-    xmin = b_box["xmin"]
-    xmax = b_box["xmax"]
-    ymin = b_box["ymin"]
-    ymax = b_box["ymax"]
-    cropped = im.crop((xmin, ymin, xmax, ymax))
+
+def preprocess_image(im_path, b_box = 'None'):
+    if b_box == 'None':
+        im = Image.open(im_path)
+        im = im.convert('RGB')
+        cropped = im
+    else:
+        im = Image.open(im_path)
+        im = im.convert('RGB')
+        xmin = b_box["xmin"]
+        xmax = b_box["xmax"]
+        ymin = b_box["ymin"]
+        ymax = b_box["ymax"]
+        cropped = im.crop((xmin, ymin, xmax, ymax))
     new_im = cropped.resize((normalized_image_width, normalized_image_height))
     arrayed = np.array(new_im)
     normalized = arrayed / 255.0
-    return normalized
-
-images_data = {}
+    reshaped = normalized.reshape(-1)
+    return reshaped
 
 def process_data():
     for subject_dir in os.listdir(annotations_dir):
@@ -44,7 +51,6 @@ def process_data():
                 ymax = int(root.find('.//object/bndbox/ymax').text)
                 b_box = {'xmin':xmin, 'xmax':xmax, 'ymin':ymin, 'ymax':ymax}
                 image = preprocess_image(image_path, b_box)
-
-                images_data[image_filename] = {'image': image, 'breed': breed, 'image_path': image_path, 'bounding_box': b_box}
+                images_data[image_filename] = {'image_data': image, 'breed': breed, 'bounding_box': b_box}
             except Exception as e:
                 print(f"Error with: {e}")
